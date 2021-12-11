f757707cec688aa89d40f8ae44e4b05237a84328
Nirbhay Singh <kumarnirbhay1986@gmail.com>
2021-12-10 21:45:33 -0600

    Dynamic left menu

---
+++ UKLinkServices/Models/LeftNavMenu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UKLinkServices.Models
+{
+    public class LeftNavMenu
+    {
+
+        public int MenueId { get; set; }
+
+        public string href { get; set; }
+
+        public string menuName { get; set; }
+        public string className { get; set; }
+
+    }
+}
---
+++ UKLinkServices/Models/RoleData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UKLinkServices.Models
+{
+    public class RoleData
+    {
+
+        public int RoleId { get; set; }
+
+        public string Role { get; set; }
+
+        public string[] menuName { get; set; }
+
+    }
+}
