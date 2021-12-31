5055ecd276c04e7efd142113a3453e36371ef2fc
Nirbhay Singh <kumarnirbhay1986@gmail.com>
2021-12-30 23:14:25 -0600

    Fix upload any type file

--- UKLinkServices/Models/Broadcast.cs
+++ UKLinkServices/Models/Broadcast.cs
@@ -24,20 +24,5 @@ namespace UKLinkServices.Models
         public string FileName { get; set; }
         public byte[] FileContent { get; set; }
     }
-    public class FileViewModel
-    {
-        /// helpful in combination with @key to make list rendering more efficient
-        public Guid Id { get; set; }
-        public String Name { get; set; }
-        public String ContentType { get; set; }
-        public Int64 Size { get; set; }
-        public Byte[] Content { get; set; }
-        public String ImageDataUrl { get; set; }
-
-        public FileViewModel()
-        {
-            Id = Guid.NewGuid();
-        }
-
-    }
+   
 }
