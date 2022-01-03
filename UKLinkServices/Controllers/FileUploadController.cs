579eeaec01080599c93aa21fef74bf3f7d7bd656
Nirbhay Singh <kumarnirbhay1986@gmail.com>
2022-01-02 19:17:46 -0600

    Fix

--- UKLinkServices/Controllers/FileUploadController.cs
+++ UKLinkServices/Controllers/FileUploadController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.IO;
- 
+using System.Data;
 
 namespace BlazorProgressiveUpload.Server.Controllers
 {
---
+++ UKLinkServices/Models/CsvFile.cs
@@ -0,0 +1,15 @@
+﻿
+namespace UKLinkServices.Models
+{
+    public class CsvFile
+    {
+        public string HouseNumber { get; set; }
+        public string StreetBuildingName { get; set; }
+        public string PrincipalStreet { get; set; }
+        public string City { get; set; }
+        public string PostodeOutCode { get; set; }
+        public string PostCodeInCode { get; set; }
+    }
+
+    
+}
